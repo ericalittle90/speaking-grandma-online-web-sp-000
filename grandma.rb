da9e4b3fede1c283edaf3cb17a6920aedb732733
Erica Little <eteney90@gmail.com>
2019-11-09 17:54:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(input)
 if input != input.upcase
    "HUH?! SPEAK UP, SONNY!"
+ elsif input == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
 elsif input == input.upcase
    "NO, NOT SINCE 1938!"
-elsif input == "I LOVE YOU GRANDMA!"
-   "I LOVE YOU TOO PUMPKIN!"
 end
 end
