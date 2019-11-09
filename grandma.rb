704ba40e4b52fc71afbaa7127f6f64aabdbbeb0a
Erica Little <eteney90@gmail.com>
2019-11-09 17:48:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
-if "#{phrase}" == downcase
+if "#{phrase}" == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
-elsif "#{phrase}" == upcase
+elsif "#{phrase}" == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 elsif "#{phrase}" == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
